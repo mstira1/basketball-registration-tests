16a5ca24ae0c7e877e1436f5186ece45d1867466
mstira1 <137098737+mstira1@users.noreply.github.com>
2025-04-21 17:19:18 +0200

    TestRunner.java

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,15 +1,18 @@
 package runner;
-import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources",
-        glue = {"steps"}, //
-        plugin = {"pretty", "html:target/cucumber-reports"},
+        glue = "steps",
+        plugin = {
+                "pretty",
+                "html:target/cucumber-reports/cucumber.html",
+                "json:target/cucumber-reports/cucumber.json"
+        },
         monochrome = true
 )
-
 public class TestRunner {
 }
